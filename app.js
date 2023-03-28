af065e1fae56c88f8de90b658aa27e382ab9f172
mateipruteanu <mateipruteanu1@gmail.com>
2023-03-28 17:14:43 +0300

    synchronizing our code

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-var http = require('http');
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response
-  res.end(); //end the response
-}).listen(3000, function(){
- console.log("server start at port 3000"); //the server object listens on port 3000
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(302, { Location: "/about.html" });
+    res.end();
+  } else if (req.url === "/about.html") {
+    fs.readFile("./about.html", (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading about.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
