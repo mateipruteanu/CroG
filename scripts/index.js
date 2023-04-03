575def13e23ef1a0c6aaaa6a7d22c7d37af5204a
mateipruteanu <mateipruteanu1@gmail.com>
2023-04-03 15:10:45 +0300

    made a basic version of the search page

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("keyup", (key) => {
+  if (key.code === "Enter") {
+    window.location.href = `./search.html?search=${encodeURIComponent(
+      searchInput.textContent
+    )}`;
+  }
+});
