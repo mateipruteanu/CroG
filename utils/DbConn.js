f0954170397e2dea0d001cba5359b801eb85cf1e
Darius Dinu <dariusdinu19@gmail.com>
2023-05-08 17:42:15 +0300

    Added the database connection credentials for AWS DB.

--- utils/DbConn.js
+++ utils/DbConn.js
@@ -1,9 +1,10 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'test',
-    user: 'root',
-    password: 'root'
+
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'database-1.cl4lhu896l24.eu-north-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'crogcrog',
 });
 
 connection.connect(function(err) {
