c8e3f09718f2f45247175558bb9106aab0e20764
mateipruteanu <mateipruteanu1@gmail.com>
2023-05-08 17:45:07 +0300

    updated DB connection + DB test
    [worked as team on this]

--- utils/DbConn.js
+++ utils/DbConn.js
@@ -1,9 +1,10 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'test',
-    user: 'root',
-    password: 'root'
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'database-1.cl4lhu896l24.eu-north-1.rds.amazonaws.com',
+    port: '3306',
+    database: 'admin',
+    user: 'admin',
+    password: 'crogcrog',
 });
 
 connection.connect(function(err) {
@@ -13,4 +14,9 @@ connection.connect(function(err) {
     };
 
     console.log('Connected as id' + connection.threadId);
+});
+
+connection.query('SHOW TABLES', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows);
 });
