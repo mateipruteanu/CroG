0d59775fdd7542ddb7d4415fa47e726746a2f3e2
mateipruteanu <mateipruteanu1@gmail.com>
2023-05-09 15:56:41 +0300

    updated db connection again (transferred to google cloud)

--- utils/DbConn.js
+++ utils/DbConn.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
-let pool = mysql.createPool({
-    connectionLimit: 10,
+let connection = mysql.createConnection({
     host: "34.116.255.231",
     port: "3306",
     database: "root",
@@ -18,7 +17,6 @@ function connect(){
     });
 }
 
-
 connection.connect(function(err) {
     if (err){
         console.log('Error connecting to Db' + err.stack);
@@ -32,20 +30,5 @@ connection.query('SHOW TABLES', function(err, rows, fields) {
     if (err) throw err;
     console.log('The solution is: ', rows);
 });
-
-// pool connection
-let pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "34.116.255.231",
-    port: "3306",
-    database: "root",
-    user: "root",
-    password: "k9RL^9p#2RHC^u",
-});
-
-
-
-
-connection.end();
 
 module.exports = connection;
