e8a9164a7b839ae938958485a203a1e3ef34bc72
Birladeanu Rares <birladeanurares@gmail.com>
2023-06-15 11:43:50 +0300

    more code cleanup

--- myservice/myservice.js
+++ myservice/myservice.js
@@ -1,32 +1,31 @@
 const mysql = require("mysql");
 let connection = mysql.createConnection({
-  host: "34.116.255.231",
-  port: "3306",
-  database: "root",
-  user: "root",
-  password: "k9RL^9p#2RHC^u",
+    host: "34.116.255.231",
+    port: "3306",
+    database: "root",
+    user: "root",
+    password: "k9RL^9p#2RHC^u",
 });
-function connect() {
-  connection.connect(function (err) {
-    if (err) {
-      console.log("Error connecting to Db" + err.stack);
-      return;
-    }
 
-    console.log("Connected as id" + connection.threadId);
-  });
+function connect() {
+    connection.connect(function (err) {
+        if (err) {
+            console.log("Error connecting to Db" + err.stack);
+            return;
+        }
+
+        console.log("Connected as id" + connection.threadId);
+    });
 }
 
 connection.query("SHOW TABLES", function (err, rows, fields) {
-  if (err) throw err;
-  console.log("The solution is: ", rows);
+    if (err) throw err;
+    console.log("The solution is: ", rows);
 });
 
-
-
 connection.query("SELECT * FROM users", function (err, rows, fields) {
-  if (err) throw err;
-  console.log("The solution is: ", rows);
+    if (err) throw err;
+    console.log("The solution is: ", rows);
 });
 
 // TODO: get a specific user
--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,3 @@
-// Desc: Array of routes for the app
-const pages = [ '/', '/addresource', '/login', '/signup', '/auth', '/search' ];
+const pages = ['/', '/addresource', '/login', '/signup', '/auth', '/search'];
 
 module.exports = pages;
